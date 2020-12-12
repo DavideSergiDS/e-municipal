b4404ceef63b17536eae455259894db7d5540f1f
davide-sergi <davidesergi.ds@gmail.com>
2020-12-12 08:21:35 +0100

    Started development REST API spring app + docs; data-model moved under  open-api specs folder

---
+++ web-services/rest-api/code/0.1.0/specs/src/test/java/com/emunicipal/restapispecsgenerator/RestapiSpecsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emunicipal.restapispecsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpecsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
